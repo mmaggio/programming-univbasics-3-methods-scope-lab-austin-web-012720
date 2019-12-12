244cf8919d12e8c631f5eac7394ce0c165672822
Francis Michael Maggio <bluebassist2002@yahoo.com>
2019-12-12 00:27:58 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,11 @@ def toadstool
   status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts status
 end
+
+def link
+  phrase = "It's Dangerous To Go Alone! Take This."
+  puts phrase
+end
+
+def any_phrase (insert_phrase)
+  
